439731eee0bfd097e3a22f9b96d43f4439500446
k4ik <kaikleal1234@gmail.com>
2024-07-04 14:25:42 -0300

    feat: remove user-select

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/images/logo.png';
 function Header() {
 	return (
 		<header className="bg-blue-600 h-16 flex justify-center items-center">
-			<img src={logo} alt="logo" className="w-56" />
+			<img src={logo} alt="logo" className="w-56 select-none" />
 		</header>
 	)
 }
