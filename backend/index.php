6f39c1934b3a5d6d6297111733aa37f3eeaa2102
k4ik <kaikleal1234@gmail.com>
2024-07-06 18:38:44 -0300

    fix: routes

--- backend/index.php
+++ backend/index.php
@@ -1,32 +1,35 @@
 <?php
-require "vendor/autoload.php"
+require "vendor/autoload.php";
 use Controller\WeatherController;
+use Controller\HelloController;
 
-$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\ConfigureRoutes $r) {
-  $r->addRoute('POST', '/location', 'postLocation');
-  $r->addRoute('GET', '/data', 'getData');
+$dispatcher = FastRoute\simpleDispatcher(function(FastRoute\RouteCollector $r) {
+    $r->addRoute('POST', '/weather', 'WeatherController@getData');
+    $r->addRoute('GET', '/helloworld', 'HelloController@helloWorld');
 });
 
 $httpMethod = $_SERVER["REQUEST_METHOD"];
 $uri = $_SERVER["REQUEST_URI"];
 
 if (false !== $pos = strpos($uri, '?')) {
-  $uri = substr($uri, 0, $pos);
+    $uri = substr($uri, 0, $pos);
 }
 $uri = rawurldecode($uri);
 
 $routeInfo = $dispatcher->dispatch($httpMethod, $uri);
 switch ($routeInfo[0]) {
-  case FastRoute\Dispatcher::NOT_FOUND:
-    header("HTTP/1.0 404 Not Found");
-    break;
-  case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
-    $allowedMethods = $routeInfo[1];
-    header("HTTP/1.0 405 Method Not Allowed");
-    break;
-  case FastRoute\Dispatcher::FOUND:
-    $handler = $routeInfo[1];
-    $vars = $routeInfo[2];
-    call_user_func([new WeatherController(), $handler], $vars);
-    break;
+    case FastRoute\Dispatcher::NOT_FOUND:
+        header("HTTP/1.0 404 Not Found");
+        break;
+    case FastRoute\Dispatcher::METHOD_NOT_ALLOWED:
+        $allowedMethods = $routeInfo[1];
+        header("HTTP/1.0 405 Method Not Allowed");
+        break;
+    case FastRoute\Dispatcher::FOUND:
+        $handler = $routeInfo[1];
+        $vars = $routeInfo[2];
+        list($controller, $method) = explode('@', $handler);
+        $controller = "Controller\\$controller";
+        call_user_func([new $controller(), $method], $vars);
+        break;
 }
